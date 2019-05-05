18a48e3f2b7c60d4c9c25ec63c3c9ad5e2a8ca54
Chuks Festus <cutogu@gmail.com>
2019-05-05 13:15:55 +0100

    setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import {normalize} from "polished"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const GlobalStyle = createGlobalStyle`
+	${normalize()}
+  body {
+    font-family: 'Open Sans', sans-serif;
+    max-width: 100vw;
+  }
+`;
+
+ReactDOM.render(
+	<Router>
+		<App />
+		<GlobalStyle />
+	</Router>,
+	document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
